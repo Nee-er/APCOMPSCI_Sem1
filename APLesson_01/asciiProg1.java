67022bbd99bddf81b26c42500acfe35d51e1dcb8
Nee-er <BoatJ7022@sduhsd.lan>
2016-09-02 14:01:52 -0700

    Added Files

---
+++ APLesson_01/asciiProg1.java
@@ -0,0 +1,13 @@
+public class asciiProg1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                            ___            ___");
+		System.out.println("                           /     \\~~~~/     \\");
+		System.out.println("          ,-----------(              ..          )");
+		System.out.println("        /                  \\____        ____/"); 
+		System.out.println("       /[                           (\\     [ (");
+		System.out.println("    ^  \\      / ____  \\      /  \\    [");
+		System.out.println("         [___]           [___]    -\'\'");
+	}
+}
---
+++ APLesson_01/asciiProg2.java
@@ -0,0 +1,7 @@
+public class asciiProg2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("     @ .. @ \n     (- - - -) \n  (  >___<  ) \n ^^  ~~  ^^");
+	}
+}
