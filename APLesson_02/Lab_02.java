b8bf01405b51c7ae7ebf932c79e92f46bc20e852
Nee-er <spencer@theboatfamily.com>
2016-09-07 14:35:51 -0700

    Lesson and Lab_02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,19 @@
+public class Lab_02
+{
+	public static void main (String[]args)
+	{
+		int num1 = 62;
+		int num2 = 74;
+		int product = num1 * num2;
+		
+		System.out.println(product);
+		
+		int name = GordonRobinson;
+		int address = 123SesameStreet
+		int city = NewYork;
+		int state = NewYork;
+		int zip = 10128;
+		
+		System.out.println("name \n address, city, state \n zip");
+	}
+}
