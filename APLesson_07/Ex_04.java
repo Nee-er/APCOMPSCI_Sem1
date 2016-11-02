be37fa6bf80e58a65e6d27e75b32749d28ffbda2
Nee-er <spencer@theboatfamily.com>
2016-11-02 14:36:32 -0700

    finish 7

--- APLesson_07/Ex_04.java
+++ APLesson_07/Ex_04.java
@@ -10,15 +10,30 @@ public class Ex_04
 		System.out.println("Enter a sentence: ");
 		sentence = kb.nextLine();
 		replace();
-		System.out.println("With @s... " + sentence);
+		System.out.println("With @s, !s, $s, and 0s... " + sentence);
 	}
 	
 	public static void replace()
 	{
-		String words = sentence;
 		while(sentence.indexOf("a") > 0)
 		{
-			words = words.substring(0, words.indexOf("a")) + words.substring(words.indexOf("a"));
+			sentence = sentence.substring(0, sentence.indexOf("a")) + "@" + sentence.substring(sentence.indexOf("a") + 1);
+		}
+		while(sentence.indexOf("s") > 0)
+		{
+			sentence = sentence.substring(0, sentence.indexOf("s")) + "$" + sentence.substring(sentence.indexOf("s") + 1);
+		}
+		while(sentence.indexOf("i") > 0)
+		{
+			sentence = sentence.substring(0, sentence.indexOf("i")) + "!" + sentence.substring(sentence.indexOf("i") + 1);
+		}
+		while(sentence.indexOf("o") > 0)
+		{
+			sentence = sentence.substring(0, sentence.indexOf("o")) + "0" + sentence.substring(sentence.indexOf("o") + 1);
+		}
+		while(sentence.indexOf("e") > 0)
+		{
+			sentence = sentence.substring(0, sentence.indexOf("e")) + "3" + sentence.substring(sentence.indexOf("e") + 1);
 		}
 	}
 }
