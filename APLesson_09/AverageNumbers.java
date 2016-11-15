b7ea11b3236ed74406262fdb29d18c04d9fc9505
Nee-er <spencer@theboatfamily.com>
2016-11-15 14:34:37 -0800

    started on 9

---
+++ APLesson_09/AverageNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AverageNumbers
+{
+	public static void main(String[]args)
+	{
+		int[]numbers = new int[10];
+		Scanner kb = new Scanner(System.in);
+		
+		for(int i = 0; i < numbers.length; i++)
+		{
+			numbers[i] = (int)(Math.random() * 100) + 1;
+		}
+	}
+	
+	public static void reverse(String[]n)
+	{
+		
+	}
+}
