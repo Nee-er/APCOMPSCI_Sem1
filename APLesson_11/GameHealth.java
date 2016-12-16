a8c79434e412e2583bf7ed8b17eaf2510cd321fb
Nee-er <spencer@theboatfamily.com>
2016-12-16 13:24:49 -0800

    finished 11.1

--- APLesson_11/GameHealth.java
+++ APLesson_11/GameHealth.java
@@ -1,34 +1,66 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
-public class GameGun
+public class GameHealth
 {
 	static int HEALTHLOAD = 6;
 	static String[] health = new String[HEALTHLOAD];
-	static int healthcount;
+	static int healthCount;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		String turn = "";
 		int damage = 0;
-		int ammount = 0;
-		healthcount = 6;
+		int amount = 0;
+		healthCount = 6;
 		
-		while(!turn.equals("Q") && healthcount > 0)
+		while(!turn.equals("Q") && healthCount > 0)
 		{
 			System.out.println("Your turn! Hit Enter when ready:");
-			turn = kb.next();
-			
+			turn = kb.nextLine();
+			damage = (int)(Math.random()*2)+1;
+			amount = (int)(Math.random()*6)+1;
+			System.out.println(takeDamage(damage, amount));
+			printClip();
 		}
+		
+		System.out.println("You died!");
 	}
 	
-	public static String takeDamage()
+	public static String takeDamage(int d, int a)
 	{
+		if(d == 1)
+		{
+			healthCount = healthCount - a;
+			return "Took damage x" + a + "!";
+		}
+		else if(healthCount + a < HEALTHLOAD)
+		{
+			healthCount = healthCount + a;
+		}
+		else
+		{
+			healthCount = HEALTHLOAD;
+		}
 		
+		return "Power Up x" + a + "!";
 	}
 	
 	public static void printClip()
 	{
-		
+		String output = "Health: \t";
+		for(int i = 0; i < HEALTHLOAD; i++)
+		{
+			if(i < healthCount)
+			{
+				health[i] = "@";
+			}
+			else
+			{
+				health[i] = "[]";
+			}
+			output = output + health[i];
+		}
+		System.out.println(output);
 	}
 }
