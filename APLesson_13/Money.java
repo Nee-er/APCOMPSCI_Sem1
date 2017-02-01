d97dfbd18b8f15eae7e228c16b0fda3c68dafc57
Nee-er <spencer@theboatfamily.com>
2017-02-01 14:33:33 -0800

    started on lesson_13

---
+++ APLesson_13/Money.java
@@ -0,0 +1,23 @@
+public abstract class Money
+{
+	private String name;
+	private double value;
+	private int count;
+	
+	public Money()
+	{
+		this.name = "";
+		this.value = 0;
+	}
+	
+	public Money(String n, double v)
+	{
+		this.name = n;
+		this.value = v;
+	}
+	
+	public String toString()
+	{
+		return "Your " + name + " is worth $" + value + ".\n";
+	}
+}
